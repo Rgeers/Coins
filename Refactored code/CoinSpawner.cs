1e3d5c7737b2c9dcca26d11b99034aebfae62bb1
Rgeers <rik.geersing@gmail.com>
2018-01-22 07:10:17 +0100

    Replaced CoinSpawner with refactored code

--- Refactored code/CoinSpawner.cs
+++ Refactored code/CoinSpawner.cs
@@ -1,45 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CoinSpawner : MonoBehaviour
-{
-    public Transform coinPrefab;
-    private Vector3 spawnPosition, mousePosition;
-    private int coinCounter = 0, totalCoins = 0;
-    void Start()
-    {
-        coinCounter = 5;
-        spawnPosition = transform.position;
-        spawnPosition.y += 0.3f;
+public class CoinSpawner : MonoBehaviour {
+    [SerializeField] private Transform _coinPrefab;
+    private Vector3 _spawnPosition, _mousePosition;
+    private int _coinCounter = 0, _totalCoins = 0;
+
+    private void Start() {
+        _coinCounter = 5;
+        _spawnPosition = transform.position;
+        _spawnPosition.y += 0.3f;
     }
 
-   void OnMouseDown()
-    {
-       if (totalCoins > 0 && coinCounter > 0)
-       {
-           RaycastHit hit;
-           Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-           if (Physics.Raycast(ray, out hit))
-           {
-               if (hit.transform.name == "CoinSpawner")
-               {
-                   StartCoroutine("CountCoins");
-                   PlayerPrefs.SetInt("Player Score", (PlayerPrefs.GetInt("Player Score")-1));
-                   Instantiate(coinPrefab, hit.point, coinPrefab.transform.rotation);
-               }
-
-           }
-       }
+    private void OnMouseDown() {
+        if (_totalCoins > 0 && _coinCounter > 0) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.transform.name == "CoinSpawner") {
+                    StartCoroutine("CountCoins");
+                    PlayerPrefs.SetInt("Player Score", (PlayerPrefs.GetInt("Player Score") - 1));
+                    Instantiate(_coinPrefab, hit.point, _coinPrefab.transform.rotation);
+                }
+            }
+        }
     }
-   IEnumerator CountCoins()
-   {
-       coinCounter--;
-       yield return new WaitForSeconds(5);
-       coinCounter++;
-   }
-    void Update()
-   {
-       totalCoins = PlayerPrefs.GetInt("Player Score");
-       PlayerPrefs.SetInt("Coin Counter", coinCounter);
-   }
+
+    private IEnumerator CountCoins() {
+        _coinCounter--;
+        yield return new WaitForSeconds(5);
+        _coinCounter++;
+    }
+
+    private void Update() {
+        _totalCoins = PlayerPrefs.GetInt("Player Score");
+        PlayerPrefs.SetInt("Coin Counter", _coinCounter);
+    }
 }
